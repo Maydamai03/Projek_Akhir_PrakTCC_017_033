141d830d7b2fff1b36a888eecde9226bcf043161
Maydamai03 <jagad.damai0303@gmail.com>
2025-05-25 14:04:13 +0700

    pakai baseURL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // config.js
 // const baseURL = "http://localhost:5000";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://be-projekakhir-17-33-296685597625.us-central1.run.app";
